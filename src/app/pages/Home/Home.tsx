15a81a399d4d87d9138d91722909adc1bf7d2479
Alan <pinhoneibor32@gmail.com>
2024-08-26 15:51:20 -0600

    chore: audio card component was created

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Icon, Text } from '@alanpinhon/deep-feels-ui';
 import { AudioCardLarge } from '../../../components/AudioCardLarge/AudioCardLarge';
+import { AudioCard } from '../../../components/AudioCard/AudioCard';
 import './HomeStyles.css';
 
 export const Home = () => {
@@ -17,13 +18,22 @@ export const Home = () => {
 
       <div className="feels-component"></div>
 
-      <Text style={{alignSelf: 'self-start'}} variant='h3'>Sonido sugerido del día</Text>
+      <Text style={{alignSelf: 'self-start', margin: '1rem 0'}} variant='h3'>Sonido sugerido del día</Text>
 
       <AudioCardLarge/>
 
       <div className="subtitle-container">
         <Text variant='h3'>Sonidos recientes</Text>
         <Text variant='sm'>Ver más</Text>
+      </div>
+
+      <div className="audios-container">
+        <AudioCard/>
+        <AudioCard/>
+        <AudioCard/>
+        <AudioCard/>
+        <AudioCard/>
+        <AudioCard/>
       </div>
     </div>
   )
---
+++ src/components/AudioCard/AudioCard.tsx
@@ -0,0 +1,16 @@
+import { colors, Icon, Text } from "@alanpinhon/deep-feels-ui"
+import './AudioCardStyles.css';
+
+export const AudioCard = () => {
+  return (
+    <div className="audio-card-container">
+      <div className="audio-card-info">
+        <div className="text-info">
+          <Text color={colors.neutralColors.white} variant='label'>Audio Title</Text>
+          <Text color={colors.neutralColors.white} variant='sm'>00 min</Text>
+        </div>
+        <Icon name='PlayIcon' size='lg' stroke={colors.neutralColors.white}/>
+      </div>
+    </div>
+  )
+}
